b5d5cf8d86502e637d867a073239b2c4886f74fd
SofiaNords <sofia.k.nordstrom.com>
2024-12-10 07:47:57 +0100

    Add logging & tracking to BookStoreContext

--- BookStore/Model/BookStoreContext.cs
+++ BookStore/Model/BookStoreContext.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace BookStore;
 
@@ -28,8 +29,109 @@ public partial class BookStoreContext : DbContext
     public virtual DbSet<Store> Stores { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Initial Catalog=BookStore;Integrated Security=True;Trust Server Certificate=True; Server SPN=localhost");
+    {
+        var connectionString = new SqlConnectionStringBuilder()
+        {
+            ServerSPN = "localhost",
+            InitialCatalog = "CityDB",
+            IntegratedSecurity = true,
+            TrustServerCertificate = true
+        }.ToString();
+
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .LogTo(
+                message => MyLogger(message),
+                new[] { DbLoggerCategory.Database.Command.Name },
+                LogLevel.Information,
+                DbContextLoggerOptions.Level | DbContextLoggerOptions.LocalTime
+            )
+            .EnableSensitiveDataLogging();
+    }
+
+    private void MyLogger(string message)
+    {
+        var lines = message.Split('\n');
+
+        Console.WriteLine();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i == 0) Console.ForegroundColor = ConsoleColor.Blue;
+            if (i == 1) Console.ForegroundColor = ConsoleColor.DarkGray;
+            if (i == 2) Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(lines[i]);
+        }
+
+        Console.ResetColor();
+        Console.WriteLine();
+    }
+
+    public void PrintChangeTrackerDebugInfo(string? caption = null, bool autoDetectChanges = true)
+    {
+        if (caption is not null)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(caption);
+            Console.ResetColor();
+        }
+
+        if (autoDetectChanges)
+        {
+            this.ChangeTracker.DetectChanges();
+        }
+
+        var debugInfo = this.ChangeTracker.DebugView.LongView;
+
+        var lines = debugInfo.Split("\n");
+
+        foreach (var line in lines)
+        {
+            var status = line.Trim('\r');
+            if (status.EndsWith("Deleted"))
+            {
+                Console.Write(status[..^7]);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Deleted");
+                Console.ResetColor();
+            }
+            else if (status.EndsWith("Modified"))
+            {
+                Console.Write(status[..^8]);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Modified");
+                Console.ResetColor();
+            }
+            else if (status.EndsWith("Unchanged"))
+            {
+                Console.Write(status[..^9]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Unchanged");
+                Console.ResetColor();
+            }
+            else if (status.Contains("Modified Originally"))
+            {
+                var stringStart = status.Split("Modified Originally").First();
+                var newValue = stringStart.Trim().Split(" ").Last();
+                var oldValue = status.Split("Modified Originally").Last();
+                var indexOfNewValue = stringStart.IndexOf(newValue);
+
+                Console.Write(stringStart[..indexOfNewValue]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(newValue);
+                Console.ResetColor();
+                Console.Write(" Modified Originally");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(oldValue);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine(status);
+            }
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
