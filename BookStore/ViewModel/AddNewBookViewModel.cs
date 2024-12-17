0f5cbbc157a17128d9f4e7e46793d1f043294eaf
SofiaNords <sofia.k.nordstrom.com>
2024-12-17 09:35:49 +0100

    Remove unnecessary usings

--- BookStore/ViewModel/AddNewBookViewModel.cs
+++ BookStore/ViewModel/AddNewBookViewModel.cs
@@ -1,12 +1,6 @@
 ﻿using BookStore.Command;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace BookStore.ViewModel
