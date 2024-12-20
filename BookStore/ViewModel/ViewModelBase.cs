fb5fd5c1522225dd081f4f4c5d73779654c869e7
SofiaNords <sofia.k.nordstrom.com>
2024-12-20 12:24:56 +0100

    Update ViewModelBase to public

--- BookStore/ViewModel/ViewModelBase.cs
+++ BookStore/ViewModel/ViewModelBase.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace BookStore.ViewModel
 {
-    class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
